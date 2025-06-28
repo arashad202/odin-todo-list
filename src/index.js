d27c6ac9b2c4027d119a71e3790477eaa29b164d
arashad202 <ahm.rashad.202@gmail.com>
2025-06-28 10:19:21 +0300

    Project_reset

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 import "./styles.css";
-import {nav} from "./modules/nav";
-import {main} from "./modules/main";
 
-const myApp = document.querySelector('#app');
-myApp.classList.add("grid", "grid-cols-[1fr_4fr]");
-
-myApp.appendChild(nav);
-myApp.appendChild(main);
 
 
--- src/modules/main.js
+++
@@ -1,5 +0,0 @@
-
-const main = document.createElement("main");
-main.classList.add("h-screen", "border", "border-black");
-
-export { main }
